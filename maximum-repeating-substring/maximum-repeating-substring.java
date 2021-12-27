6575c43f9c73a1faf0919ae9061aa30318e6a82e
Carry0077 <67675936+Carry0077@users.noreply.github.com>
2021-12-27 18:56:06 +0530

    Time: 1 ms (69.70%), Space: 37 MB (96.50%) - LeetHub

---
+++ maximum-repeating-substring/maximum-repeating-substring.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxRepeating(String sequence, String word) {
+        String temp=word;
+        int count=0;
+     while(sequence.contains(temp)){
+         count++;
+         temp=temp+word;
+     }
+        return count;
+    }
+}
