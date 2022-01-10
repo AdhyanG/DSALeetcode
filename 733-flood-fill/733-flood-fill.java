862d1c6b295d98fd7ac2c187a29e54b7625274ac
Carry0077 <67675936+Carry0077@users.noreply.github.com>
2022-01-10 11:26:32 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (77.31%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        if(image[sr][sc]==newColor)return image;
+        flood(image,sr,sc,image[sr][sc],newColor);
+        return image;
+        
+    }
+    public void flood(int[][] image, int sr, int sc,int color, int newColor){
+        if(sr<0||sc<0||sr>=image.length||sc>=image[0].length||image[sr][sc]!=color)return;
+    
+    image[sr][sc]=newColor;
+    flood(image,sr-1,sc,color,newColor);
+     flood(image,sr+1,sc,color,newColor);
+     flood(image,sr,sc-1,color,newColor);
+     flood(image,sr,sc+1,color,newColor);
+    }
+}
