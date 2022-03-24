8fda5df14f63332a9a4e7ad508dad00645973703
AdhyanG <67675936+AdhyanG@users.noreply.github.com>
2022-03-24 10:57:59 +0530

    Added solution - LeetHub

--- Union of two arrays - GFG/union-of-two-arrays.java
+++ Union of two arrays - GFG/union-of-two-arrays.java
@@ -53,7 +53,7 @@ class Solution{
     public static int doUnion(int a[], int n, int b[], int m) 
     {
         //Your code here
-     HashSet<Integer> set = new HashSet<>();
+Set<Integer> set = new HashSet<>();
     
        for(int i=0;i<n;i++) {
            set.add(a[i]);
