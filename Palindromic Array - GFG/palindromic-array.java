e2cbc46be4da4bd3a0b1354ea772cebe401551ed
AdhyanG <67675936+AdhyanG@users.noreply.github.com>
2022-04-05 12:29:39 +0530

    Added solution - LeetHub

--- Palindromic Array - GFG/palindromic-array.java
+++ Palindromic Array - GFG/palindromic-array.java
@@ -22,20 +22,21 @@ class GfG
 {
 	public static int palinArray(int[] a, int n)
            {
-                  //add code here.
-                  for(int i=0;i<n;i++){
-                      int og=a[i];
-                      int res=0;
-                      while(og>0){
-                          int temp=og%10;
-                           og=og/10;
-                           res=res*10+temp;
-                         
-                      }
-                     if(res!=a[i]){
-                        return 0;
-                      }
-                  }
-              return 1;
+            
+
+
+int r, i;
+   for(i=0; i<n; i++){
+   int q = a[i], sum = 0;
+   while(q != 0)
+   {
+       r = q%10;
+       sum = sum*10 + r;
+       q = q/10;
+   }
+   if(sum != a[i])
+   return 0;
+  }
+   return 1;      //add code here.
            }
 }
