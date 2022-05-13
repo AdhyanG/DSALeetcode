a32e8443a7856ca811712322b26e0cedf098bc6b
AdhyanG <67675936+AdhyanG@users.noreply.github.com>
2022-05-13 11:11:17 +0530

    Time: 11 ms (69.17%), Space: 55.1 MB (58.59%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,26 +1,39 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        
-        if(intervals.length<=1){
+        if(intervals.length<=0){
             return intervals;
         }
-        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
-        List<int[]> output_arr=new ArrayList<>();
-        int[] curr_inter=intervals[0];
-        output_arr.add(curr_inter);
+        Arrays.sort(intervals,(arr1,arr2)->Integer.compare(arr1[0],arr2[0]));
+        List<int[]> out_arr=new ArrayList<>();
+        int[] curr_int=intervals[0];
+        out_arr.add(curr_int);
         for(int[] interval:intervals){
-            int curr_begins=curr_inter[0];
-            int curr_end=curr_inter[1];
-            int next_begin=interval[0];
+            int curr_start=curr_int[0];
+            int curr_end=curr_int[1];
+            int next_start=interval[0];
             int next_end=interval[1];
-            if(curr_end>=next_begin){
-                curr_inter[1]=Math.max(curr_end,next_end);
+            
+            if(curr_end>=next_start){
+                curr_int[1]=Math.max(curr_end,next_end);
             }
             else{
-                curr_inter=interval;
-                output_arr.add(curr_inter);
+                curr_int=interval;
+                out_arr.add(curr_int);
             }
         }
-        return output_arr.toArray(new int[output_arr.size()][]);
+        return out_arr.toArray(new int[out_arr.size()][]);
+        
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+//[1,3],[2,6],[8,10]
