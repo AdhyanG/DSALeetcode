ef570f7a4ea7c444a1df52f13494b6e0d7737424
AdhyanG <67675936+AdhyanG@users.noreply.github.com>
2022-07-08 17:59:56 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (61.87%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,11 +1,30 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int index=0;
-      return linearSearch(nums,target,index);
-    }
-    public int linearSearch(int[] nums,int target,int index){
-        if(index==nums.length)return -1;
-        if(nums[index]==target)return index;
-        return linearSearch(nums,target,index+1);
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(target==nums[mid]){
+                return mid;
+            }
+            if(nums[start]<=nums[mid]){
+                if(target>=nums[start]&&target<nums[mid]){
+                    end=mid-1;
+                }
+                else{
+                    start=mid+1;
+                }
+            }
+            else{
+                if(target>=nums[mid]&&target<=nums[end]){
+                    start=mid+1;
+                }
+                else{
+                    end=mid-1;
+                }
+            }
+        }
+        return -1;
+        
     }
 }
