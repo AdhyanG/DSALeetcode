4edbe05e389ac4fb1e9fa3117d567a8487d3e655
AdhyanG <67675936+AdhyanG@users.noreply.github.com>
2022-07-30 23:08:52 +0530

    Time: 3 ms (60.70%), Space: 43.3 MB (34.19%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.java
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.java
@@ -1,23 +1,21 @@
 class Solution {
     public List<String> findAndReplacePattern(String[] words, String pattern) {
-        //get the permutation map of 
         List<String> list=new ArrayList<>();
         String result=get(pattern);
         for(String word:words){
-            String result_2=get(word);
-                if(result_2.equals(result)){
-                    list.add(word);
-                }
-            }
-          return list;  
+            String answer=get(word);
+            if(answer.equals(result))list.add(word);
+            
         }
-       
+        return list;
+    }
     
-    private String get(String string){
+    
+    private String get(String s){
         HashMap<Character,Integer> map=new HashMap<>();
-        int i=0;
         StringBuilder sb=new StringBuilder();
-        for(char c:string.toCharArray()){
+        int i=0;
+        for(char c:s.toCharArray()){
             if(!map.containsKey(c)){
                 map.put(c,i++);
                 sb.append(i);
