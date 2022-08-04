547f3a4f16d5fef4dd051411b6b6f1dbee10a81e
AdhyanG <67675936+AdhyanG@users.noreply.github.com>
2022-08-04 14:18:24 +0530

    Time: 1 ms (99.77%), Space: 43.8 MB (84.80%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,22 +1,24 @@
 class Solution {
     public int trap(int[] height) {
-        int i=0;
-        int j=height.length-1;
-        int maxLeft=0;
-        int maxRight=0;
-        int water=0;
-        while(i<j){
-            if(height[i]<=height[j]){
-                maxLeft=Math.max(maxLeft,height[i]);
-                water +=maxLeft-height[i];
-                i++;
-            }
-            else{
-                maxRight=Math.max(maxRight,height[j]);
-                water +=maxRight-height[j];
-                j--;
-            }
+        int sum=0;
+        int[] lmx=new int[height.length];
+        int[] rmx=new int[height.length];
+        lmx[0]=height[0];
+        for(int i=1;i<lmx.length;i++){
+            lmx[i]=Math.max(lmx[i-1],height[i]);
         }
-        return water;
+        rmx[rmx.length-1]=height[height.length-1];
+        for(int i=height.length-2;i>=0;i--){
+            rmx[i]=Math.max(rmx[i+1],height[i]);
+        }
+        int[] res=new int[height.length];
+        for(int i=0;i<res.length;i++){
+            res[i]=Math.min(lmx[i],rmx[i])-height[i];
+        }
+        for(int i=0;i<res.length;i++){
+            sum=sum+res[i];
+        }
+        return sum;
+        
     }
 }
